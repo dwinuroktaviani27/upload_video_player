1343bf26e07bd5cc7c1e2031136d1b99f82e8d8f
dwinuroktaviani27 <1941720239@student.polinema.ac.id>
2022-09-16 17:02:39 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# upload_video_player
+
+A new Flutter project.
+
+## Upload File from Camera
+![Praktikum1](images/prak1.gif)
---
+++ android/app/src/main/kotlin/com/example/upload_video_player/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.upload_video_player
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
